65af082b7d05ddab634e119a6be1fa1754637cdd
Makechi <makechieric9@gmail.com>
2023-11-09 11:53:30 +0300

    Switched to vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Login } from "./components";
+
+function App() {
+    return (
+        <>
+            <Login />
+        </>
+    );
+}
+
+export default App;
